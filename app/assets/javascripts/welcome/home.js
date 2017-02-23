f666a4a02d61b59fcb3eaf8cce4585a68c23db4f
meo84 <oopsiday@gmail.com>
2017-02-23 16:59:56 +0100

    fix layout

--- app/assets/javascripts/welcome/home.js
+++ app/assets/javascripts/welcome/home.js
@@ -1,6 +1,9 @@
 $(function() {
   $('.filmtheme').click(function() {
+    $(this).toggleClass('filmtheme-zoom');
+    $(this).siblings().toggle();
+    $(this).parent().siblings().toggle();
     $(this).children('.front').toggle();
-    $(this).children('.back').toggle();
+    $(this).children('.back').slideToggle();
   });
 });

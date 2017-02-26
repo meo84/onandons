141631d9b14a0497d49b7655c42d82161ae8b180
meo84 <oopsiday@gmail.com>
2017-02-26 14:48:19 +0100

    center text on filmtheme front cards

--- app/assets/javascripts/welcome/home.js
+++ app/assets/javascripts/welcome/home.js
@@ -1,6 +1,7 @@
 $(function() {
   $('.filmtheme').click(function() {
     $(this).toggleClass('filmtheme-zoom');
+    $(this).toggleClass('filmtheme-centercontent');
     $(this).siblings().toggle();
     $(this).parent().siblings().toggle();
     $(this).children('.front').toggle();
